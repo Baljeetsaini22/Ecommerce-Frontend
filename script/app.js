2e5f8ae6f7175aa3d120ddc1306ab838eda5b909
Baljeetsaini22 <baljeetsaini7440@gmail.com>
2025-06-01 23:56:09 +0530

    header

--- script/app.js
+++ script/app.js
@@ -1,1 +1,16 @@
 console.log("E-Commerce Website Loaded");
+/**
+ * @description this is harmburger
+ * @returns get menu icon on mobile view
+ * @function harmburger()
+*/
+function hamburger(){
+    document.addEventListener("DOMContentLoaded", () => { 
+        const hamburger = document.getElementById("hamburger");
+        const navMenu = document.querySelector(".nav-menu");
+        hamburger.addEventListener("click", () => {
+            navMenu.classList.toggle("open");
+        });
+    });
+}
+hamburger()
