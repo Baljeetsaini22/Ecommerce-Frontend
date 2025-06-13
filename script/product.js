4a92105f8b6cd7739511295a10762457c86dc065
Baljeetsaini22 <baljeetsaini7440@gmail.com>
2025-06-13 22:54:33 +0530

    newJs

---
+++ script/product.js
@@ -0,0 +1,34 @@
+function ProductDetails() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const productId = urlParams.get("id");
+  //   console.log(productId)
+  fetch("https://fakestoreapiserver.reactbd.com/walmart")
+    .then((response) => response.json())
+    .then((data) => {
+      const product = data.find(
+        (item) => String(item._id) === String(productId)
+      );
+
+      if (product) {
+        document.getElementById("product-container").innerHTML = `
+            <h2>${product.title}</h2>
+            <img src="${product.image}" alt="${product.title}" width="150" />
+            <p>Price: ₹ ${Math.floor(product.price * 80)} <del> ₹ ${Math.floor(
+          product.oldPrice * 80
+        )} </del></p>
+            <p>${product.des}</p>
+            <p>${product.category}</p>
+            
+          `;
+      } else {
+        document.getElementById("product-container").innerText =
+          "Product not found.";
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching product:", error);
+      document.getElementById("product-container").innerText =
+        "Failed to load product.";
+    });
+}
+ProductDetails()
